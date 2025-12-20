40963189a97c35479e6defa31adcaf545c417010
sporotofpoorety <pereira.m.biel@gmail.com>
2025-12-20 00:16:38 -0300

    Fixed a small mistake

--- src/main/java/net/daveyx0/primitivemobs/mixins/MyCoreMod.java
+++ src/main/java/net/daveyx0/primitivemobs/mixins/MyCoreMod.java
@@ -1,7 +1,11 @@
+package net.daveyx0.primitivemobs.mixins;
+
 import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 @IFMLLoadingPlugin.Name("MyCoreModName")
 @IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
 public class MyCoreMod implements IFMLLoadingPlugin, IEarlyMixinLoader { }
+
+
 
